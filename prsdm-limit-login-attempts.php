9e675e7485e55ea9e8b03bd558f1e9bafb6ae8ea
paolo.bertin <paolo.bertin.62@gmail.com>
2024-08-24 22:39:41 +0200

    aggiunto autolad dipendenze

--- prsdm-limit-login-attempts.php
+++ prsdm-limit-login-attempts.php
@@ -80,8 +80,9 @@ class Plugin
      */
     private function require_files()
     {
+        require_once __DIR__ . '/vendor/autoload.php';
+
         require_once __DIR__ . '/autoload.php';
-        
         $autoloader = new Autoloader();
         $autoloader->register();
     }
